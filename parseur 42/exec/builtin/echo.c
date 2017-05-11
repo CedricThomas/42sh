7d510ce588310dddacdf49b394bb6609588c0c28
Thibaut Cornolti <thibaut.cornolti@epitech.eu>
2017-05-11 16:38:00 +0200

    oui

--- parseur 42/exec/builtin/echo.c
+++ parseur 42/exec/builtin/echo.c
@@ -5,17 +5,20 @@
 ** Login   <thibaut.cornolti@epitech.eu>
 ** 
 ** Started on  Thu May 11 13:22:48 2017 Thibaut Cornolti
-** Last update Thu May 11 15:50:33 2017 Thibaut Cornolti
+** Last update Thu May 11 16:37:44 2017 Thibaut Cornolti
 */
 
 #include "syntax.h"
 #include "exec.h"
+#include "my.h"
 
 void		builtin_echo(t_command *cmd, t_status *status, t_info *info)
 {
   int		i;
 
   i = 0;
+  UNUSED(status);
+  UNUSED(info);
   while (cmd->argv[++i])
     {
       my_putstr(cmd->argv[i]);
