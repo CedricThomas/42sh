c6aebe9bc93a2fab682f77143673be66a827352f
marin.brunel <marin.brunel@epitech.eu>
2017-05-12 11:22:24 +0200

    mklmk

--- parseur 42/main.c
+++ parseur 42/main.c
@@ -5,32 +5,49 @@
 ** Login   <cedric@epitech.net>
 ** 
 ** Started on  Sat Oct 22 10:31:05 2016 Cédric Thomas
-** Last update Thu May 11 18:28:47 2017 maje
+** Last update Fri May 12 11:21:50 2017 maje
 */
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include "my.h"
 #include "get_next_line.h"
 #include "syntax.h"
 #include "exec.h"
 #include "my_printf.h"
 
+static void	git(t_info *info)
+{
+  DIR		*dir;
+  struct dirent	*dirent;
+
+  if ((dir = opendir(info->pwd)) == NULL)
+    return ;
+  while ((dirent = readdir(dir)) != NULL)
+    {
+      if (my_strcmp(".git", dirent->d_name) == 0)
+	my_printf("\033[36;01mgit\033[00m  ");
+    }
+  closedir(dir);
+}
+
 static void	extract(t_info *info)
 {
-  char	*home;
-  int	i;
+  char		*home;
+  int		i;
 
   i = my_strlen(info->pwd);
   home = getkey(info->env, "HOME", 0);
   if (home && !my_strcmp(info->pwd, home))
-    my_printf("\033[36;01m~\033[00m");
+    my_printf("\033[36;01m~\033[00m  ");
   else
     {
       while (i > 0 && info->pwd[i] != '/')
 	i -= 1;
       if (i != 0)
 	i += 1;
-      my_printf("\033[36;01m%s\033[00m", info->pwd + i);
+      my_printf("\033[36;01m%s\033[00m  ", info->pwd + i);
     }
 }
 
@@ -41,7 +58,7 @@ static void     print_prompt(t_info *info)
   else
     my_printf("\033[32;01m➜  \033[00m");
   extract(info);
-  my_printf(" \033[31;01m \033[00m");
+  git(info);
 }
 
 int		main(int ac, char **av, char **env)
