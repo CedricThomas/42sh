f94bb16a7f575c0217a9b3eba738bc680245c673
Bastien <bastien.rinck@epitech.eu>
2017-05-15 22:03:55 +0200

    builtin comme tcsh, valeur de retour NON set

--- parseur42/exec/builtin/alias.c
+++ parseur42/exec/builtin/alias.c
@@ -5,7 +5,7 @@
 ** Login   <rectoria@epitech.net>
 ** 
 ** Started on  Mon May 15 15:48:20 2017 Bastien
-** Last update Mon May 15 18:46:21 2017 Bastien
+** Last update Mon May 15 22:02:39 2017 Bastien
 */
 
 #include <stdio.h>
@@ -25,40 +25,74 @@ static int	my_strtablen(char **tab)
   return (i < 0 ? 0 : i);
 }
 
-void		show_cmd(t_info *info, char *str)
+void		show_cmd(t_info *info, char *str, int size)
 {
   int	i;
 
   i = -1;
-  while (info->alias[++i].link)
-    if (!strcmp(info->alias[i].link, str))
+  while (size && info->alias[++i].link)
+    {
+      if (str && !strcmp(info->alias[i].link, str))
+	{
+	  printf("%s\n", info->alias[i].value);
+	  return ;
+	}
+      else if (!str)
+	printf("%s\t%s\n", info->alias[i].link, info->alias[i].value);
+    }
+}
+
+int		verify_exist(t_info *info, char *str, char *value)
+{
+  int	i;
+
+  i = -1;
+  while (info->alias && info->alias[++i].link)
+    if (!my_strcmp(info->alias[i].link, str))
       {
-	printf("%s=%s\n", info->alias[i].link, info->alias[i].value);
-	return ;
+	info->alias[i].value = value;
+	return (0);
       }
+  return (1);
+}
+
+char	*get_whole_alias(t_command *cmd)
+{
+  int	i;
+  char	*temp;
+
+  i = 1;
+  temp = NULL;
+  while (cmd->argv[++i])
+    {
+      temp = my_strcatdup(temp, cmd->argv[i], 1);
+      if (cmd->argv[i + 1])
+	temp = my_strcatdup(temp, " ", 1);
+    }
+  return (temp);
 }
 
 void		builtin_alias(t_command *cmd, t_status *status, t_info *info)
 {
-  int		i;
+  int		nb_arg;
+  char		*temp;
   static int	size = 0;
 
-  (void)status;
-  if (my_strtablen(cmd->argv) != 2)
-    return ;
-  i = my_cstrlen(cmd->argv[1], '=');
-  if (fnmatch("*=\"*\"", cmd->argv[1], 0) || cmd->argv[1][i] != '=')
+  UNUSED(status);
+  nb_arg = my_strtablen(cmd->argv);
+  if (nb_arg <= 2)
     {
-      if (size)
-	show_cmd(info, cmd->argv[1]);
+      show_cmd(info, cmd->argv[1], size);
       return ;
     }
-  if (!(info->alias = realloc(info->alias, sizeof(t_alias) * (size + 2))))
-    return ;
-  memset(&info->alias[size], 0, sizeof(t_alias) * 2);
-  cmd->argv[1][i] = 0;
-  info->alias[size].link = strdup(cmd->argv[1]);
-  cmd->argv[1][i] = '=';
-  info->alias[size].value = strdup(cmd->argv[1] + i + 1);
-  size += 1;
+  temp = get_whole_alias(cmd);
+  if (verify_exist(info, cmd->argv[1], temp))
+    {
+      if (!(info->alias = realloc(info->alias, sizeof(t_alias) * (size + 2))))
+	return ;
+      memset(&info->alias[size], 0, sizeof(t_alias) * 2);
+      info->alias[size].link = strdup(cmd->argv[1]);
+      info->alias[size].value = temp;
+      size += 1;
+    }
 }
