9278ec29418a1cfbd94965198ff406400fe55608
marin.brunel <marin.brunel@epitech.eu>
2017-05-18 19:43:57 +0200

    mlkmk

--- binds/history.c
+++ binds/history.c
@@ -5,7 +5,7 @@
 ** Login   <cedric.thomas@epitech.eu>
 ** 
 ** Started on  Thu May 18 19:13:06 2017 Cédric THOMAS
-** Last update Thu May 18 19:14:43 2017 Cédric THOMAS
+** Last update Thu May 18 19:43:13 2017 maje
 */
 #include <curses.h>
 #include <termio.h>
@@ -19,9 +19,10 @@
 
 int		down_arrow(t_keypad *keypad)
 {
+  t_history	hist;
 
-
-
+  if (keypad->system->info->histo->current->next == NULL)
+    return (-1);
 }
 
 int		up_arrow(t_keypad *keypad)
