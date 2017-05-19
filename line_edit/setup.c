36d329b9ad7f0161b69da3e5958c13bac7fd230b
marin.brunel <marin.brunel@epitech.eu>
2017-05-19 19:34:03 +0200

    mklmk$

--- line_edit/setup.c
+++ line_edit/setup.c
@@ -5,7 +5,7 @@
 ** Login   <cedric.thomas@epitech.eu>
 **
 ** Started on  Fri Apr 21 22:15:37 2017
-** Last update Fri May 19 19:32:41 2017 maje
+** Last update Fri May 19 19:33:59 2017 maje
 */
 #include <unistd.h>
 #include <stdlib.h>
@@ -34,7 +34,7 @@ int		my_set_term(t_keypad *keypad)
     return (1);
   if ((smkx = tigetstr("smkx")) == (char *)-1)
     return (1);
-  my_putstr(smkx);
+   my_putstr(smkx);
   return (0);
 }
 
