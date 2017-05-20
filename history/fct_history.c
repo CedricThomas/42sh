68749c01e0052d28304b5f94501e6e61d6f444a9
Thibaut Cornolti <thibaut.cornolti@epitech.eu>
2017-05-20 16:13:04 +0200

    oui

--- history/fct_history.c
+++ history/fct_history.c
@@ -5,10 +5,11 @@
 ** Login   <thibaut.cornolti@epitech.eu>
 ** 
 ** Started on  Sat May 20 15:39:02 2017 Thibaut Cornolti
-** Last update Sat May 20 16:04:51 2017 Thibaut Cornolti
+** Last update Sat May 20 16:10:09 2017 Thibaut Cornolti
 */
 
 #include <termio.h>
+#include <stdlib.h>
 #include "syntax.h"
 #include "exec.h"
 #include "my.h"
@@ -16,31 +17,40 @@
 
 char	*history_fct_exclam(char *src, int idx, t_history_info *history)
 {
-  delete_nbchar(src, 1, idx);
-  insert_str(src, history->end->cmd, idx, 0);
+  if (delete_nbchar(src, 1, idx) == NULL ||
+      insert_str(src, history->end->cmd, idx, 0) == NULL)
+    exit(84);
   return (src);
 }
 
 char	*history_fct_dollar(char *src, int idx, t_history_info *history)
 {
-  
+  if (delete_nbchar(src, 1, idx) == NULL ||
+      insert_str(src, history->end->cmd, idx, 0) == NULL)
+    exit(84);  
   return (src);
 }
 
 char	*history_fct_colon(char *src, int idx, t_history_info *history)
 {
-
+  if (delete_nbchar(src, 1, idx) == NULL ||
+      insert_str(src, history->end->cmd, idx, 0) == NULL)
+    exit(84);
   return (src);
 }
 
 char	*history_fct_dash(char *src, int idx, t_history_info *history)
 {
-
+  if (delete_nbchar(src, 1, idx) == NULL ||
+      insert_str(src, history->end->cmd, idx, 0) == NULL)
+    exit(84);
   return (src);
 }
 
 char	*history_fct_number(char *src, int idx, t_history_info *history)
 {
-
+  if (delete_nbchar(src, 1, idx) == NULL ||
+      insert_str(src, history->end->cmd, idx, 0) == NULL)
+    exit(84);
   return (src);
 }
