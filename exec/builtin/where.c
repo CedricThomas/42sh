5664ac277c76bd05fa944bdeef3713f036e55d89
Bastien <bastien.rinck@epitech.eu>
2017-05-20 14:33:52 +0200

    where

--- exec/builtin/where.c
+++ exec/builtin/where.c
@@ -5,17 +5,104 @@
 ** Login   <thibaut.cornolti@epitech.eu>
 ** 
 ** Started on  Thu May 18 23:57:02 2017 Thibaut Cornolti
-** Last update Sat May 20 13:39:07 2017 Thibaut Cornolti
+** Last update Sat May 20 14:32:47 2017 Bastien
 */
 
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include "syntax.h"
 #include "exec.h"
 #include "my.h"
 
+static int	verify_alias(t_info *info, char *str)
+{
+  int	i;
+
+  i = -1;
+  while (info->alias && info->alias[++i].link)
+    if (!strcmp(str, info->alias[i].link))
+      {
+	printf("%s is aliased to %s\n", str, info->alias[i].value);
+	return (1);
+      }
+  return (0);
+}
+
+static int	verify_builtin(t_info *info, char *str)
+{
+  int	i;
+
+  i = -1;
+  while (++i < BUILTINS_NB)
+    if (!strcmp(str, info->builtins[i]))
+      {
+	printf("%s is a shell built-in\n", str);
+	return (1);
+      }
+  return (0);
+}
+
+int	check_inpath(char **tab, char *str)
+{
+  char	*path;
+  int	i;
+
+  i = -1;
+  while (tab[++i])
+    {
+      path = my_strcatdup(tab[i], "/", 0);
+      path = my_strcatdup(path, str, 1);
+      if (!access(path, F_OK))
+	{
+	  printf("%s\n", path);
+	  free(path);
+	  return (1);
+	}
+      free(path);
+    }
+  return (0);
+}
+
+int	verify_path(t_info *info, char *str)
+{
+  char	*path;
+  char	**tab;
+  int	value;
+
+  if (!(path = getkey(info->env, "PATH", 1)))
+    return (0);
+  if (!(tab = my_split(path, ':')))
+    {
+      free(path);
+      return (0);
+    }
+  free(path);
+  value = check_inpath(tab, str);
+  free_tab(tab);
+  return (value);
+}
+
 void		builtin_where(t_command *cmd, t_status *status, t_info *info)
 {
-  UNUSED(cmd);
+  int		i;
+  int		finded;
+
   UNUSED(status);
-  info->exit_value = 1;
+  i = 0;
+  info->exit_value = cmd->argv[1] ? 0 : 1;
+  if (!cmd->argv[1])
+    {
+      printf("where: Too few arguments.\n");
+      return ;
+    }
+  while (cmd->argv[++i])
+    {
+      finded = 0;
+      finded += verify_alias(info, cmd->argv[i]);
+      finded += verify_builtin(info, cmd->argv[i]);
+      finded += verify_path(info, cmd->argv[i]);
+      info->exit_value = (!info->exit_value && !finded) ? 1 : info->exit_value;
+    }
 }
